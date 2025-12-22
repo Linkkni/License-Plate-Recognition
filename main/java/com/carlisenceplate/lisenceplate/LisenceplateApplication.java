2bfc072256de8314eea7932fbf87c631dfb86823
fixah <nguyenduonglinhso1@gmail.com>
2025-12-22 15:08:47 +0800

    Create Project

---
+++ main/java/com/carlisenceplate/lisenceplate/LisenceplateApplication.java
@@ -0,0 +1,13 @@
+package com.carlisenceplate.lisenceplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisenceplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LisenceplateApplication.class, args);
+	}
+
+}
