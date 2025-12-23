2cfc327cc90724f5ff13dd6e2678d54382a98a9c
fixah <nguyenduonglinhso1@gmail.com>
2025-12-23 17:14:30 +0800

    switch tess4J to easyOCR

--- main/java/com/carlisenceplate/lisenceplate/storageService/LicensePlateService.java
+++ main/java/com/carlisenceplate/lisenceplate/storageService/LicensePlateService.java
@@ -4,15 +4,26 @@ package com.carlisenceplate.lisenceplate.storageService;
 import com.carlisenceplate.lisenceplate.entity.Plate;
 import com.carlisenceplate.lisenceplate.repository.LicensePlateRepository;
 import net.sourceforge.tess4j.Tesseract;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -22,53 +33,40 @@ public class LicensePlateService {
     @Autowired
     private LicensePlateRepository licensePlateRepository;
 
-    //this function read image original path -> response image path has processed
+    // URL của Python Server
+    private final String PYTHON_API_URL = "http://localhost:5000/detect";
 
+    public String extractPlateNumber(String imagePath) {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            File file = new File(imagePath);
 
+            // 1. Tạo Request gửi file sang Python
+            HttpPost uploadFile = new HttpPost(PYTHON_API_URL);
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
 
+            // Đính kèm file ảnh
+            builder.addBinaryBody("image", file, ContentType.IMAGE_JPEG, file.getName());
+            HttpEntity multipart = builder.build();
+            uploadFile.setEntity(multipart);
 
+            // 2. Gửi và nhận kết quả
+            try (CloseableHttpResponse response = httpClient.execute(uploadFile)) {
+                HttpEntity responseEntity = response.getEntity();
+                String responseString = EntityUtils.toString(responseEntity);
 
+                // 3. Parse JSON từ Python trả về
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode rootNode = mapper.readTree(responseString);
 
-
-
+                String status = rootNode.path("status").asText();
+                if ("success".equals(status)) {
+                    return rootNode.path("plate").asText(); // Trả về biển số: "GR35383"
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
-            }
-            return "No Plate Found";
         }
-    private BufferedImage matToBufferedImage(Mat mat) throws IOException {
-        MatOfByte mob = new MatOfByte();
-        Imgcodecs.imencode(".jpg", mat, mob);
-        return ImageIO.read(new ByteArrayInputStream(mob.toArray()));
-    }
-    public Map<String, Object> checkVehicle(String plateNumber){
-
-        Map<String, Object> response = new HashMap<>();
-
-        //Finding in Database
-        Optional<Plate> plateOpt = licensePlateRepository.findByPlateNumber(plateNumber);
-
-        if(plateOpt.isPresent()){
-            Plate plate = plateOpt.get();
-
-            //checking status
-            if("ALLOWED".equalsIgnoreCase(plate.getStatus())) {
-                response.put("status", "SUCCESS");
-                response.put("message", "Gate Opened. Welcome " + plate.getVehicleOwner());
-                response.put("data", plate);
-            }else {
-                response.put("status", "BLOCKED");
-                response.put("message", "Access Denied. Vehicle is not allowed.");
-                response.put("data", plate);
-            }
-        }else {
-            //Not sign up yet(Stranger car)
-            response.put("status", "UNKNOWN");
-            response.put("message", "Unregistered Vehicle. Please contact security.");
-            response.put("data", plateNumber);
-        }
-        return response;
+        return "No Plate Found";
     }
 
     public Plate registerVehical(Plate plate){
